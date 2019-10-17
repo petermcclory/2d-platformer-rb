93004259eb057bce3d234447e889b77c69af663f
petermcclory <petermcclory@gmail.com>
2019-10-17 15:02:04 +0100

    General script updates

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,15 +7,38 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+         
+    }
+
+    private void FixedUpdate()
+    {
+        // Move Player Character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
     }
 }
